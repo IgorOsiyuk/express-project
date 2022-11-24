96658a3e4fac0793b7a17a20007575010c91d3ad
Ihar <ihar@prezzee.com>
2022-11-24 15:16:56 +0300

    add morgan logger

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import { fileURLToPath } from "node:url";
 import path from "node:path"
+import morgan from "morgan"
 import planetRouter from './routes/planets/planets.router.js'
 
 const app = express()
@@ -12,7 +13,7 @@ const __dirname = path.dirname(__filename);
 app.use(cors({
     origin: "http://localhost:3000"
 }))
-
+app.use(morgan("combined"))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "..", 'public')))
 
